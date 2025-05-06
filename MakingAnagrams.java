56a9a51fff1211d3f801fb7ca9da9e7c6b859f24
ankush1038 <ankusharma1029@gmail.com>
2025-05-06 19:47:04 +0530

    [Ankush] Add: Number of deletions printed

---
+++ MakingAnagrams.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result2 {
+
+    /*
+     * Complete the 'makingAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(String s1, String s2) {
+        // Write your code here
+        int deletions = 0;
+        int[] freqency = new int[26];
+        for(char c :  s1.toCharArray()){
+            freqency[c - 'a']++;
+        }
+
+        for(char c : s2.toCharArray()){
+            freqency[c - 'a']--;
+        }
+
+        for(int count: freqency){
+            deletions += Math.abs(count);
+        }
+
+        return deletions;
+    }
+
+}
+
+public class MakingAnagrams {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result2.makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
