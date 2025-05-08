30c57bb29efbe66d6e5f187699d6be76e9d1706f
ankush1038 <ankusharma1029@gmail.com>
2025-05-08 13:53:23 +0530

    [Ankush] Add: Largest Rectangle found

--- LargestRectangle.java
+++ LargestRectangle.java
@@ -65,7 +65,7 @@ class Result1 {
 
 }
 
-public class Solution {
+public class LargestRectangle {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
